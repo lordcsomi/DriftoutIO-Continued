269e6401172bbdfb7eb56c3a7ae66be264c2f563
Csomi <csomos.adam.benedek@gmail.com>
2024-07-12 10:33:54 +0200

    port change for hosting

--- Driftout/server/server.js
+++ Driftout/server/server.js
@@ -5,7 +5,7 @@ var socketIO = require("socket.io");
 
 // Needs replacement upon cloud hosting?
 var publicPath = path.join(__dirname, "../client")
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8080;
 var host = process.env.HOST || '0.0.0.0';
 var app = express();
 var server = http.createServer(app);
